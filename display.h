c5caf1151ec907f824992d06765185ffe8d3a4c4
airbagy <y27duan@edu.uwaterloo.ca>
2017-07-18 16:04:39 -0400

    yayay

---
+++ display.h
@@ -0,0 +1,16 @@
+//
+//  display.hpp
+//  CC3K
+//
+//  Created by Dennis on 2017-07-18.
+//  Copyright © 2017 Dennis. All rights reserved.
+//
+
+#ifndef display_h
+#define display_h
+
+class Display {
+    
+};
+
+#endif /* display_h */
---
+++ enemy.h
@@ -0,0 +1,21 @@
+//
+//  enemy.hpp
+//  CC3K
+//
+//  Created by Dennis on 2017-07-16.
+//  Copyright © 2017 Dennis. All rights reserved.
+//
+
+#ifndef enemy_h
+#define enemy_h
+#include "thing.h"
+
+class Enemy: public Thing {
+    int hp;
+    int atk;
+    int def;
+public:
+    virtual 
+};
+
+#endif /* enemy_h */
