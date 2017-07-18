48391bc55944d5a5b1d89bda8152d03fee88f29e
Sammaraw <huangzixiao7@hotmail.com>
2017-07-18 16:46:25 -0400

    Update player.cc

--- player.cc
+++ player.cc
@@ -7,3 +7,37 @@
 //
 
 #include "player.h"
+
+#include "player.h"
+#include <iostream>
+
+using namespace std;
+
+Player::Player(string pc_type,int x, int y):Thing{pc_type.substr(0,1),x,y},onDH{false},gold{0},mKiller{false}{}
+
+Player::createPlayer(std::string type){
+  if (type == "@drow"){(need to delete?)
+     this = new Drow();
+  }else if(type == "@vampire"){
+     this = new Vampire();
+  }else if(type == "@troll"){
+     this = new Troll();
+  }else if (type == "@goblin"){
+     this = new Goblin();
+  }else if (type == "@shade"){
+      this = new Shade();
+  }
+}
+
+void setmKiller(){
+  mKiller = true;
+}
+
+
+void Player::attack(Thing* t){
+  t->hurt(this);
+}
+
+void Player::hurt(Enemy* e){
+    
+}
