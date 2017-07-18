6c724cf90611bb65c69c223bbd64e257e5b8bc58
Sammaraw <huangzixiao7@hotmail.com>
2017-07-18 16:45:44 -0400

    Update potion.cc

--- potion.cc
+++ potion.cc
@@ -7,3 +7,31 @@
 //
 
 #include "potion.h"
+
+Potion::Potion(string name,int x, int y):Thing{name, x, y}{
+  if (name == "PRH"){
+    hp = 10;
+  }else if (name == "PBA"){
+    atk = 5;
+  }else if (name == "PBD"){
+    def = 5;
+  }else if (name == "PPH"){
+    hp = -10;
+  }else if (name  == "PWA"){
+    atk = -5;
+  }else if (name == "PWD"){
+    def = -5;
+  } 
+}
+
+Potion::modify(Player* p){
+  if p->name=="@drow"{
+    p.addhp(health*1.5);                               //magfinied
+    p.addatk(atk*1.5);
+    p.adddef(def*1.5);
+ }else{
+    p.addhp(health);
+    p.addatk(atk);
+    p.addddef(def);
+ }
+}
