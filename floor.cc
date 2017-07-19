e086a48a9cbd415a06729346854414b7f546497f
airbagy <y27duan@edu.uwaterloo.ca>
2017-07-19 19:12:51 -0400

    put chambre to floor

--- floor.cc
+++ floor.cc
@@ -6,6 +6,57 @@
 //  Copyright © 2017 Dennis. All rights reserved.
 //
 
+#include <cstdlib>
 #include "floor.h"
+
+// 
+void givePos(int& x, int& y) {
+    srand(time(NULL));
+    int v = rand()%5; // generate 0~4
+    if (v == 0) {
+        int v0 = rand()%104; // chambre1 -> 4 * 26 = 104 tiles 1~104
+        int row = v0 / 26;
+        int col = v0 % 26;
+        x = row + 3;
+        y = col + 3;
+        
+    } else if (v == 1) {
+        int v1 = rand()%201; // chambre2 -> 201 tiles
+        if ((v1 >= 0) && (v1 <= 45)) {
+            x = v1 / 23 + 3;
+            y = v1 % 23 + 39;
+        } else if ((v1 >= 46) && (v1 <= 76)) {
+            x = 5;
+            y = v1 - 46 + 39;
+        } else if ((v1 >= 77) && (v1 <= 110)) {
+            x = 6;
+            y = v1 - 77 + 39;
+        } else {
+            x = (v1 - 111) / 15 + 7;
+            y = (v1 - 111) % 15 + 61;
+        }
+        
+    } else if (v == 2) {
+        int v2 = rand()%36; // chambre3 -> 36 tiles -> 3 * 12
+        x = v2 / 12 + 10;
+        y = v2 % 12 + 38;
+        
+    } else if (v == 3) {
+        int v3 = rand()%147; // chambre4 -> 147 tiles -> 21 * 7
+        x = v3 / 21 + 15;
+        y = v3 % 21 + 4;
+        
+    } else {
+        int v4 = rand()%150; // chanbre5 -> 150 tiles
+        if ((v4 >= 0) && (v4 <= 32)) {
+            x = v4 / 11 + 16;
+            y = v4 % 11 + 65;
+        } else {
+            x = (v4 - 33) / 39 + 19;
+            y = (v4 - 33) % 39 + 37;
+        }
+    }
+}
+
 
 Floor :: Floor(std::string fileName, Display* dis): height{25}, width{79}, 
