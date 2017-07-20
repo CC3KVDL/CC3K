5cf285f73c4674a569beda991ba1f06f122fdb2a
VeronicaHan <topveronicaa@gmail.com>
2017-07-19 20:22:27 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -14,8 +14,6 @@
 using namespace std;
 
 
-
-
 int main(int argc, const char * argv[]) {
     cout << "Please choose your race or choose quit." << endl;
     string pc_type;
@@ -52,8 +50,63 @@ int main(int argc, const char * argv[]) {
            f.readMap(argv[1]); 
         }
        
+        // print the map and message
         f.print();
         me->printStatus();
-        cout << f.getMes();
+        cout << f.getMes() << "Player enters floor: " << floor << endl;
+        
+       
+        // read in command
+        string command;
+        bool moveEnemy = true;
+        while (cin >> command) {
+            
+            // break out loop conditions
+            if (me->getHp() <= 0) {
+                cout << "You have scored: " << me->getGold() << endl;
+                return;
+            }
+            // NOTICE: if at stair ???  && if floor == 5 win the game
+            
+            
+            // interprates commands
+            if (command == "no" || 
+                command == "so" || 
+                command == "ea" || 
+                command == "we" || 
+                command == "ne" ||
+                command == "nw" ||
+                command == "se" ||
+                command == "sw") {
+               f.movePlayer(me, command);
+            } else if (command == "u") {
+                string dir;
+                cin >> dir;
+                f.usePotion(me, dir);
+            } else if (command == "a") {
+                string dir;
+                cin >> dir;
+                f.attackEnemay(me, dir);
+            } else if (command == "f" ) {
+                moveEnemy = false;
+            } else if (command == "r" ) {
+                // NOTICE: restart ???
+            } else if (command == "q" ) {
+               cout << "Game Over" << endl;
+               return;
+            } //  NOTICE: invalide command????
+            
+            // update information and move enemies
+            f.check();
+            if (moveEnemy) {
+                f.moveEnemies();
+            }
+            
+            // print new map  NOTICE: clear old map????
+            f.print();
+            me->printStatus();
+            cout << f.getMes() << endl;
+        }
     }
 }
+
