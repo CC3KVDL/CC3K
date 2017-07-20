ebfd3eef8f32a0a11428dbdfebe397439d1e7711
Sammaraw <huangzixiao7@hotmail.com>
2017-07-19 20:25:46 -0400

    Update player.h

--- player.h
+++ player.h
@@ -17,15 +17,17 @@ class Player: public Thing{
     int atk;
     int def;
     int gold;
-    bool OnDH; // Boolean value to indicate whether "me" is stepping on a Dragon Hoard
-    bool MKiller; // True if "I" have killed merchant
+    Thing* On; // Thing that the Player is stepping on
+    bool mKiller; // True if "I" have killed merchant
 public:
-    Player(std::string pc_type = "", int x=-1, int y=-1);
+    Player(std::string pc_type = "", int x=-1, int y=-1, Thing* On);
     virtual ~Player();
     Player *createPlayer(std::string pc_type);
     virtual void attack(Enemy &e);
     virtual void hurt(Enemy &e);
+    
     void gainGold(int amount); // This is called whenever "I" steps on a pile of gold or "I" killed an enemy
+    void getOn();
     void getGold();
     void getHp();
     void printStatus(); // Print out the status of "me"
