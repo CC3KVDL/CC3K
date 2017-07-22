937f91774d41b58e7431b00654440c161988c18c
Sammaraw <huangzixiao7@hotmail.com>
2017-07-22 15:13:06 -0400

    Update goblin.cc

--- goblin.cc
+++ goblin.cc
@@ -11,13 +11,17 @@
 Goblin::Goblin(): Player{"@goblin", 110, 15, 20}{}
 
     
-void Goblin::hurt(Enemy &e) {
+string Goblin::hurt(Enemy &e) {
   int hurt = (100 / (100 + def))* e.getAtk();
+  string m;
   if (e.getName()=="Oorcs"){
     addHp(-hurt*1.5);
+    m = m + e.getName.substr(1) +  " deals " + to_string(hurt) + " damage to PC . ";
   }else{
     addHp(-hurt);
+    m = m + e.getName.substr(1) +  " deals " + to_string(hurt) + " damage to PC . ";
   }
+    return m;
 }
 
 Goblin::~Goblin(){}
