73160e9985ca594932b1a5380ec30c6a8fc73333
VeronicaHan <topveronicaa@gmail.com>
2017-07-22 14:51:49 -0400

    Update thing.h

--- thing.h
+++ thing.h
@@ -34,7 +34,6 @@ public:
     virtual void hurt(Thing &t);
     virtual void use(Thing * t);
     virtual int getHp();
-    virtual int getDamage();
 
     // Virtual Method for Gold
     virtual void setOwner(Thing *t);
